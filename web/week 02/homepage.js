c1513c7120b65152b0fc6d34794fb2ec04fd9181
jjreed05 <jjreed05@gmail.com>
2018-01-18 23:39:19 -0700

    removed directory

--- web/week 02/homepage.js
+++
@@ -1,5 +0,0 @@
-function insertTranslation() {
-    document.getElementById('romaji').innerHTML = "Mezasubeki tokoro ni, chikamichi wa sonzai shinai.";
-    document.getElementById('english').innerHTML = "There are no short cuts to any place worth going";
-
-}
