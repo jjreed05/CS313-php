e7d7c8099429993fd313e92179ff4cd53a8851f5
jjreed05 <jjreed05@gmail.com>
2018-01-18 23:20:28 -0700

    Created a homepage for week 2

---
+++ web/week 02/assignments.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Assignments</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="homepage.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    
+</head>
+<body>
+    <?php
+    include_once("navbar.php");
+    ?>
+    
+    <!-- First Panel -->
+    <div class="container">
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                <h1>Coming Soon</h1>
+            </div>
+        </div>
+</body>
+</html>
---
+++ web/week 02/homepage.js
@@ -0,0 +1,5 @@
+function insertTranslation() {
+    document.getElementById('romaji').innerHTML = "Mezasubeki tokoro ni, chikamichi wa sonzai shinai.";
+    document.getElementById('english').innerHTML = "There are no short cuts to any place worth going";
+
+}
