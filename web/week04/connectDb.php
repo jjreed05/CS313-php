2ab93d670a8ad92a3c87e24036a27f8dd18edf0c
jjreed05 <jjreed05@gmail.com>
2018-02-10 18:34:50 -0700

    trying to get this to work

--- web/week04/connectDb.php
+++ web/week04/connectDb.php
@@ -4,8 +4,7 @@ try
 {
   $user = 'qqabikskgthgpos';
   $password = '2f276e59f82527e2b73e3fe471815954c4364fbbe39e054c54aee16923eef91c';
-  $db = new PDO('postgres://qqabikskgthgpo:2f276e59f82527e2b73e3fe471815954c4364fbbe39e054c54aee16923eef91c@ec2-54-225-103-255.compute-1.amazonaws.com:5432/d4hr7otnaq0hmo
-', $user, $password);
+  $db = new PDO('postgres://qqabikskgthgpo:2f276e59f82527e2b73e3fe471815954c4364fbbe39e054c54aee16923eef91c@ec2-54-225-103-255.compute-1.amazonaws.com:5432/d4hr7otnaq0hmo', $user, $password);
     
 }
 catch (PDOException $ex)
