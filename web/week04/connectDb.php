dd0d911c61458f2e075f615f7a903e2268bfa554
jjreed05 <jjreed05@gmail.com>
2018-02-10 18:37:57 -0700

    still trying to get this to work

--- web/week04/connectDb.php
+++ web/week04/connectDb.php
@@ -2,9 +2,17 @@
 
 try
 {
-  $user = 'qqabikskgthgpos';
-  $password = '2f276e59f82527e2b73e3fe471815954c4364fbbe39e054c54aee16923eef91c';
-  $db = new PDO('postgres://qqabikskgthgpo:2f276e59f82527e2b73e3fe471815954c4364fbbe39e054c54aee16923eef91c@ec2-54-225-103-255.compute-1.amazonaws.com:5432/d4hr7otnaq0hmo', $user, $password);
+$dbUrl = getenv('DATABASE_URL');
+
+$dbopts = parse_url($dbUrl);
+
+$dbHost = $dbopts["host"];
+$dbPort = $dbopts["port"];
+$dbUser = $dbopts["user"];
+$dbPassword = $dbopts["pass"];
+$dbName = ltrim($dbopts["path"],'/');
+
+$db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
     
 }
 catch (PDOException $ex)
