26c186060fd7e30bd0276bb9c15e78782b6f5ac9
jjreed05 <jjreed05@gmail.com>
2018-06-27 13:24:46 -0600

    Changing the color

--- web/week04/browse.php
+++ web/week04/browse.php
@@ -34,10 +34,9 @@ foreach($rows as $row){
     <link rel="stylesheet" type="text/css"  href="browse.css"/>
     
     <!-- Using Bootstrap -->
-    <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-    <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
-    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-    
+    <link rel="stylesheet"href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
 </head>
 <?php
  
